e8490bd714d5a3fed79f84c04fb4829552616d5e
Anum Asif <unum.asif@gmail.com>
2019-10-12 10:13:15 -0700

    initial

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       <nav className="navbar navbar-light bg-danger fixed-top">
+        <span className="navbar-brand mb-0 h1">SafeZone</span>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
