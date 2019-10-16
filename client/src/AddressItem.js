b5043624534977862b2539c929d534fab1f9b544
Anum Asif <unum.asif@gmail.com>
2019-10-15 19:38:49 -0700

    nav styling

--- client/src/AddressItem.js
+++ client/src/AddressItem.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class AddressItem extends Component {
   render() {
     return (
-        <div className="row form-group justify-content-start">
+        <div className="row  add-form form-group justify-content-start">
             <label className="col-sm-4 col-form-label">{this.props.label}</label>
             <div className="col-xl-8">
                 <input
