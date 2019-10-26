dac9256c1f9d0e0d7ed745b57ff9b367c287c783
iTh1nk <fnchao@hotmail.com>
2019-10-25 22:14:50 -0700

    panda react-chat basic func pass

--- client/src/pages/Panda.js
+++ client/src/pages/Panda.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Widget, addResponseMessage, addLinkSnippet, addUserMessage } from 'react-chat-widget';
+import { Widget, addResponseMessage, addLinkSnippet, addUserMessage, renderCustomComponent } from 'react-chat-widget';
 
 import 'react-chat-widget/lib/styles.css';
 import logo from '../avatar.jpg';
@@ -11,34 +11,36 @@ let ioTimeout;
 class Panda extends Component {
 
     state = {
-
+        userName: "iTh1nk"
     }
 
     componentDidMount() {
         addResponseMessage("Welcome to this awesome chat!");
     }
 
-    
+    // handleNewUserMessage = (newMessage, data) => {
+    //     console.log(`New message incoming! ${newMessage}`);
+    //     addResponseMessage(data);
+    // }
 
-    handleSocketIo = newMessage => {
-        socket.emit('sendreactchat', newMessage);
+    handleSocketIo = newMessage000 => {
+        socket.removeAllListeners();
+        socket.emit('sendreactchat', this.state.userName + ": " + newMessage000);
         socket.on('recvreactchat', data => {
-            // handleNewUserMessage = (newMessage) => {
-                console.log(`New message incoming! ${newMessage}`);
-                // Now send the message throught the backend API
-            // }
-            // console.log('client receive :', [this.state.userName + this.state.conversation]);
+            // this.handleNewUserMessage(newMessage, data);
+            addResponseMessage(data);
         })
+        // socket.removeListener('recvreactchat');
     }
 
     render() {
         return (
             <div className="App">
                 <Widget
-                    // handleNewUserMessage={this.handleSocketIo}
-                    profileAvatar={logo}
-                    title="My new awesome title"
-                    subtitle="And my cool subtitle"
+                    handleNewUserMessage={this.handleSocketIo}
+                    // profileAvatar={logo}
+                    title="Safe Zone Community"
+                    subtitle="Stay Alive"
                 />
             </div>
         );
